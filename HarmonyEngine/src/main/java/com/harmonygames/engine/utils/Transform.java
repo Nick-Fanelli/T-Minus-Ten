e4ad53a4f445f2f15d30c3a984cbab3bc6023ab5
Nick <nickfanelli5@gmail.com>
2020-12-11 18:57:31 -0500

    Added in the transform classes methods and fields

--- HarmonyEngine/src/main/java/com/harmonygames/engine/utils/Transform.java
+++ HarmonyEngine/src/main/java/com/harmonygames/engine/utils/Transform.java
@@ -3,6 +3,34 @@ package com.harmonygames.engine.utils;
 public class Transform {
 
     public Vector2f position;
-    public Vector2f scale;
+    public Scale scale;
 
+    public Transform() {
+        this(new Vector2f(), new Scale());
+    }
+
+    public Transform(Vector2f position) {
+        this(position, new Scale());
+    }
+
+    public Transform(Vector2f position, Scale scale) {
+        this.position = position;
+        this.scale = scale;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Transform)) {
+            System.err.println("[Harmony Engine]: Could not use .equals with " + obj + " and Transform");
+            return false;
+        }
+
+        Transform transform = (Transform) obj;
+        return transform.position.equals(this.position) && transform.scale.equals(this.scale);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Transform: %s, %s]", this.position.toString(), this.scale.toString());
+    }
 }
