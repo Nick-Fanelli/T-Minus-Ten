8b045bfef208f2753ca3d00754872df553b8c757
Nick <nickfanelli5@gmail.com>
2020-12-13 11:03:50 -0500

    Added in the box collider

---
+++ HarmonyEngine/src/main/java/com/harmonygames/engine/gameobject/component/BoxCollider2D.java
@@ -0,0 +1,27 @@
+package com.harmonygames.engine.gameobject.component;
+
+import com.harmonygames.engine.utils.Scale;
+import com.harmonygames.engine.utils.Vector2f;
+
+import java.awt.*;
+
+public class BoxCollider2D extends Component {
+
+    private Vector2f offset;
+    private Scale scale;
+
+    public BoxCollider2D(Vector2f offset, Scale scale) {
+        this.offset = offset;
+        this.scale = scale;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+
+    }
+}
